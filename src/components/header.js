27097e3f08531e5af0b2573357055aafdad52030
Josh <34256170+ScienceOfSleep@users.noreply.github.com>
2020-09-04 20:26:46 -0600

    Added logo to header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,40 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, {useEffect, useState} from "react"
+import {css} from "@emotion/core"
+import styled from "@emotion/styled"
+import {graphql, Link, useStaticQuery} from "gatsby";
+import Navbar from "./navbar";
+import Img from "gatsby-image";
 
-const Header = () => (
-  <header>
+const Header = (props) => {
 
+  const data = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "university-of-alberta-feminists-club-logo.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return <header
+    css={css`
+          background-color: var(--fua-pink);
+          display: flex;
+        `}
+  >
+    <Img
+        fluid={data.logo.childImageSharp.fluid}
+        alt="University of Alberta Feminists Club Logo"
+        css={css`
+              width: 200px;
+              border-radius: 100%;
+            `}
+    />
+    <Navbar/>
   </header>
-)
+}
 
 export default Header
---
+++ src/pages/events.js
@@ -0,0 +1,19 @@
+import React from "react"
+import {Link, useStaticQuery} from "gatsby"
+import {css} from "@emotion/core"
+import { graphql } from "gatsby";
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Header from "../components/header";
+
+const EventsPage = () => {
+
+    return <Layout>
+        <Header/>
+        <h1>test</h1>
+    </Layout>
+}
+
+export default EventsPage
